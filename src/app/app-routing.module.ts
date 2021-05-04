10b3da8c125411fb13a8ff26b81c37d51c46571f
Emery <emerywalraet@gmail.com>
2021-05-05 00:28:46 +0200

    Create route for list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './pokemon/detail/detail.component';
 import { PokemonResolver } from './../shared/resolvers/pokemon.resolver';
+import { ListComponent } from './pokemon/list/list.component';
 
 const routes: Routes = [
+  {
+    path: 'pokemon',
+    component: ListComponent,
+  },
   {
     path: 'pokemon/:name',
     component: DetailComponent,
@@ -11,6 +16,12 @@ const routes: Routes = [
       pokemon: PokemonResolver,
     }
   },
+  {
+    path: '',
+    redirectTo: '/pokemon',
+    pathMatch: 'full',
+  },
+
 ];
 
 @NgModule({
